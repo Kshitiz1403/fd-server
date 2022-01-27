5de94f1bcf613e84099e51df995b51aca1432ab3
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-01-27 14:35:14 +0530

    initialize firebase admin sdk

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const { initializeApp } = require('firebase-admin/app');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebasePrivateKey.json");
+
+initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
