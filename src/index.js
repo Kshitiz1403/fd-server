684342fdfef58aed627891c4ec23e6e3f7dbd0b1
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-01-31 20:48:09 +0530

    add cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ require('./db/mongoose')
 const userRouter = require("./routers/user")
 const restaurantRouter = require("./routers/restaurant")
 const orderRouter = require('./routers/order')
+const cors = require("cors")
 
 const app = express()
+app.use(cors())
 
 const port = process.env.PORT || 5000
+
+// const multer = require("multer")
+
+// const upload = multer({
+//     dest:'images'
+// })
+
+// app.post('/upload',upload.single('upload') ,(req, res)=>{
+//     res.send("success!")
+// })
 
 app.use(express.json())
 
