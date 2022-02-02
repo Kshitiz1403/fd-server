5dacd1f64511f89c9004a93ffd4f9e0b59f3badc
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-02-02 20:30:59 +0530

    create an endpoint to update user cart

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,41 +26,41 @@ router.get('/users/:id', (req, res) => {
 // Updates an user with the given UID
 // Eg. => localhost:5000/users/12222221
 
-router.patch('/users/:id', async(req, res) => {
-    try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true})
-        if (!user){
+router.patch('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (!user) {
             return res.status(400).send("The user was not found")
         }
         res.send(user)
     }
-    catch(err){
+    catch (err) {
         res.status(400).send(err)
     }
 })
 
 //WARNING!!!! - validations do not run on this operation so use carefully
 //Upserts the document i.e. creates or updates an existing document  
-router.put('/users/:id', async(req, res) => {
-    try{
-        const user = await User.findOneAndUpdate({_id:req.params.id}, req.body, {upsert:true, useFindAndModify:false, new:true})
+router.put('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { upsert: true, useFindAndModify: false, new: true })
         res.send(user)
     }
-    catch(err){
+    catch (err) {
         res.status(400).send(err)
     }
 })
 
 // Deletes an user with given UID
-router.delete('/users/:id', async(req, res)=>{
-    try{
+router.delete('/users/:id', async (req, res) => {
+    try {
         const user = await User.findByIdAndDelete(req.params.id)
-        if (!user){
+        if (!user) {
             return res.status(404).send("User does not exist")
         }
         res.send(user)
     }
-    catch(err){
+    catch (err) {
         res.status(500).send(err)
     }
 })
@@ -76,6 +76,20 @@ router.post('/users/:id', async (req, res) => {
     catch (err) {
         res.status(400).send(err.message)
     }
+})
+
+// Updates an users cart with given UID
+router.patch('/users/:id/cart', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                return res.status(400).send("User does not exists")
+            }
+            user.set({ "cart": req.body })
+            return user.save()
+        })
+        .then(user => res.send(user.cart))
+        .catch(err => res.status(400).send(err))
 })
 
 module.exports = router
