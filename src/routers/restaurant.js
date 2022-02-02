30146b049bbcbe929cd458b25c50650a5190bec8
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-02-02 16:55:19 +0530

    add end point for image upload for dish

--- src/routers/restaurant.js
+++ src/routers/restaurant.js
@@ -7,6 +7,20 @@ const fs = require("fs")
 const imgbbUploader = require("imgbb-uploader");
 const { default: axios } = require("axios")
 
+const upload = multer({
+    dest: 'uploads',
+    limits: {
+        fileSize: 5000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error("Please upload an image. Supported file types are jpeg & png"))
+        }
+        cb(undefined, true)
+    }
+})
+
+
 // Get all restaurants
 router.get('/restaurants', (req, res) => {
     Restaurant.find({})
@@ -68,6 +82,31 @@ router.delete('/restaurants/:id', async (req, res) => {
         res.status(500).send(err)
     }
 })
+
+// Uploads image to a restaurants by id with image as key
+router.post('/restaurants/:id/upload', upload.single('image'), async (req, res) => {
+    let file = req.file
+
+    const ext = file.mimetype.split('/')[1];
+
+    fs.renameSync(`${file.path}`, `${file.path}.${ext}`);
+
+    const response = await imgbbUploader(process.env.IMGBB_KEY, `uploads/${file.filename}.${ext}`)
+    try {
+        const resp = await axios.patch(`https://apifd.herokuapp.com/restaurants/${req.params.id}`, {
+            imageURI: response.url
+        })
+        return res.send(resp.data)
+    }
+    catch (err) {
+        return res.status(500).send(err.message)
+    }
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+
+// ------------------------------Dishes-------------------------------------------
 
 // Create a dish by restaurant ID
 router.post('/restaurants/:id/dishes', async (req, res) => {
@@ -153,38 +192,29 @@ router.delete('/restaurants/:id/dishes/:dishID', (req, res) => {
         .catch(err => res.status(400).send(err))
 })
 
-const upload = multer({
-    dest: 'uploads',
-    limits: {
-        fileSize: 5000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error("Please upload an image. Supported file types are jpeg & png"))
-        }
-        cb(undefined, true)
-    }
-})
-
-// Uploads image to a restaurants by id with image as key
-router.post('/restaurants/:id/upload', upload.single('image'), async (req, res) => {
+// Uploads image to a dish by restaurant id and dish id with image as key
+router.post('/restaurants/:id/dishes/:dishID/upload', upload.single('image'), async (req, res) => {
     let file = req.file
-
     const ext = file.mimetype.split('/')[1];
 
     fs.renameSync(`${file.path}`, `${file.path}.${ext}`);
-
     const response = await imgbbUploader(process.env.IMGBB_KEY, `uploads/${file.filename}.${ext}`)
-    console.log(response.url)
-    try {
-        const resp = await axios.patch(`https://apifd.herokuapp.com/restaurants/${req.params.id}`, {
-            imageURI: response.url
+    console.log(response)
+
+    Restaurant.findById(req.params.id)
+        .then(restaurant => {
+            if (!restaurant) {
+                return res.status(400).send("Restaurant does not exists")
+            }
+            const dish = restaurant.dishes.id(req.params.dishID)
+            if (!dish) {
+                return res.status(400).send("Dish does not exists")
+            }
+            dish.set({ "imageURI": response.url })
+            return restaurant.save()
         })
-        return res.send(resp.data)
-    }
-    catch (err) {
-        return res.status(500).send(err.message)
-    }
+        .then(restaurant => res.send(restaurant.dishes.id(req.params.dishID)))
+        .catch(err => res.status(400).send(err))
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
